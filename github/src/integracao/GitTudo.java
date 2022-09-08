b990774c0907b8543dd528a9f9c5b7cbb78af892
luiz.camargo <luiz.camargo@JOIC05PC01.catolicasc.org.br>
2022-09-08 16:00:10 -0300

    Adicionada a classe GitTudo

---
+++ github/src/integracao/GitTudo.java
@@ -0,0 +1,20 @@
+package integracao;
+public class GitTudo {
+	private String nomeGitudo;
+	private String dataGitTudo;
+	
+	public GitTudo(String nomeGitudo, String dataGitTudo) {
+		this.nomeGitudo = nomeGitudo;
+		this.dataGitTudo = dataGitTudo;
+	}
+
+	public String getNomeGitudo() {
+		return nomeGitudo;
+	}
+
+	public String getDataGitTudo() {
+		return dataGitTudo;
+	}
+
+	
+}
