97f68ea90c1a48fa8ff96920493442da0370517f
luiz.camargo <luiz.camargo@JOIC05PC01.catolicasc.org.br>
2022-09-08 15:54:39 -0300

    Adicionada uma a classe GitTudo.

--- github/src/integracao/HelloGit.java
+++ github/src/integracao/HelloGit.java
@@ -3,9 +3,12 @@ package integracao;
 public class HelloGit {
 	
 	public static void main(String[] args) {
-
+		
+	GitTudo gt = new GitTudo("Git é tudo de bom", "08/09/2022");
 	   System.out.println("Hello Git!"); 
 	   System.out.println("Este Projeto foi clonado e alterado!");
+	   System.out.println("Clonado e commitado novamente....." + "agora com... \n" +  
+	   gt.getNomeGitudo() + " em " + gt.getDataGitTudo());
 	}   
 
 }
